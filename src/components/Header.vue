0ee452f4a1213f845a59da7ff6fcd238d3927b62
diek <d_kearney@bellaliant.net>
2020-05-29 01:49:26 -0300

    Create required components, add them to App.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,29 @@
 <!-- HTML code -->
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+</template>
 <!-- JavaScript (Vue) code -->
+<script>
+  export default {
+    name: 'Header',
+    date () {
+      return {
+        title: 'Vue CRUD Project'
+      }
+    }
+  }
+</script>
+
 <!-- CSS code -->
+!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  margin: auto;
+}
+
+header h1 {
+  font-size: 2.5em;
+}
+</style>
